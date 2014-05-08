64fad1ff0949fe2072cfed8becf43263f289ca42
Chris Moore <chrism@gaslight.co>
2014-05-08 14:39:33 -0400

    Ditch phantom for the moment

--- spec/support/javascript.rb
+++ spec/support/javascript.rb
@@ -4,4 +4,5 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
 
-Capybara.javascript_driver = ENV.fetch("JS_DRIVER", "poltergeist").to_sym
+Capybara.javascript_driver = ENV.fetch("JS_DRIVER", "selenium").to_sym
+
