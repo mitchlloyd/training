b639f029e06c4f0b0c3bf4c36aa378d28e9b84f3
Chris Moore <chrism@gaslight.co>
2014-06-03 11:14:28 -0400

    Update stats URL

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ TrainingApp::Engine.routes.draw do
 
   resources :courses, only: [:index, :show] do
     get :theme, on: :member
-    resources :registrations do
-      get :stats, on: :collection
-    end
+    resources :registrations
   end
 
   namespace :api, defaults: {format: :json} do
     resources :courses
-    resources :registrations
     resources :discount_codes, only: :show
+    resources :registrations do
+      get :stats, on: :collection
+    end
   end
 
   root to: 'courses#index'
