af7a53002a5316903f6b59a55e6ebae16124eebf
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-09-05 11:02:42 -0400

    Fix demoing a course spec

--- spec/features/demoing_a_course_spec.rb
+++ spec/features/demoing_a_course_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "Demoing a course", js: true do
   let(:classroom_page) { ClassroomPage.new }
   let(:course) { FactoryGirl.create(:course, online: true) }
-  let!(:section) { FactoryGirl.create(:section, course: course) }
+  let(:section) { FactoryGirl.create(:section, course: course) }
 
   describe "viewing restricted nav links" do
     let!(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", demo: true, section: section) }
@@ -18,7 +18,7 @@ feature "Demoing a course", js: true do
 
   describe "viewing a chapter" do
     context "demo chapter" do
-      let(:demo_chapter) { FactoryGirl.create(:chapter, title: "Demo", demo: true, code_url: "http://example.com/") }
+      let!(:demo_chapter) { FactoryGirl.create(:chapter, section: section, title: "Demo", demo: true, code_url: "http://example.com/") }
       scenario "should show the chapter" do
         classroom_page.visit_chapter(demo_chapter)
         expect(classroom_page.code_url).to eq("http://example.com/")
@@ -26,7 +26,7 @@ feature "Demoing a course", js: true do
     end
 
     context "restricted chapter" do
-      let(:restricted_chapter) { FactoryGirl.create(:chapter, title: "Restricted", demo: false, code_url: "http://example.com/") }
+      let(:restricted_chapter) { FactoryGirl.create(:chapter, section: section, title: "Restricted", demo: false, code_url: "http://example.com/") }
       scenario "should show the purchase modal" do
         classroom_page.visit_course(restricted_chapter.course) #visit the course first to prevent routing error
         classroom_page.visit_chapter(restricted_chapter)
